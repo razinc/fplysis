3c5987aae320b2584b37cc94d4ccd7d457c6ddf1
razinc <l_noir@yahoo.com>
2023-10-04 19:11:37 +0800

    combine tests into a single test

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,7 +3,7 @@ import pytest
 from custom.user import User
 
 
-def test_get_user_with_mock(monkeypatch):
+def test_init_with_mock(monkeypatch):
     async def mock_get_user(self):
         return {
             "player_first_name": "Fábio",
@@ -13,120 +13,53 @@ def test_get_user_with_mock(monkeypatch):
 
     monkeypatch.setattr(User, "get_user", mock_get_user)
 
-    user = User(user_id=4305040)
-
-    assert user.name == "Fábio, Borges"
-    assert user.team_name == "Clichy's Cleansheets"
-
-
-def test_get_picks_with_mock(monkeypatch):
     async def mock_get_picks(self):
         return {
             4: [
                 {
                     "element": 101,
-                    "position": 1,
-                    "multiplier": 1,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 203,
-                    "position": 2,
-                    "multiplier": 1,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 20,
-                    "position": 3,
-                    "multiplier": 1,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 195,
-                    "position": 4,
-                    "multiplier": 1,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 373,
-                    "position": 5,
-                    "multiplier": 1,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 108,
-                    "position": 6,
-                    "multiplier": 1,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 143,
-                    "position": 7,
-                    "multiplier": 1,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 308,
-                    "position": 8,
-                    "multiplier": 1,
-                    "is_captain": False,
-                    "is_vice_captain": True,
                 },
                 {
                     "element": 396,
-                    "position": 9,
-                    "multiplier": 1,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 211,
-                    "position": 10,
-                    "multiplier": 1,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 355,
-                    "position": 11,
-                    "multiplier": 2,
-                    "is_captain": True,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 28,
-                    "position": 12,
-                    "multiplier": 0,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 473,
-                    "position": 13,
-                    "multiplier": 0,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 316,
-                    "position": 14,
-                    "multiplier": 0,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
                 {
                     "element": 490,
-                    "position": 15,
-                    "multiplier": 0,
-                    "is_captain": False,
-                    "is_vice_captain": False,
                 },
             ]
         }
@@ -134,7 +67,11 @@ def test_get_picks_with_mock(monkeypatch):
     monkeypatch.setattr(User, "get_picks", mock_get_picks)
 
     user = User(user_id=4305040)
-
+    assert user.log_in == None
+    assert user.user_id == 4305040
+    assert user.name == "Fábio, Borges"
+    assert user.team_name == "Clichy's Cleansheets"
+    assert user.in_the_bank == "This is only available through login"
     assert user.team == [
         101,
         203,
